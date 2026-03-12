6aad77f1b66b24fa4c6a237d6f43c83fd84c7df4
AndrewL1498 <andrewlindsey498@gmail.com>
2026-03-12 17:58:48 -0500

    building components for portfolio site

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/home.module.css";
+
+
+export default function Home() {
+    return (
+       <section id="home" className={styles.home}>
+            <h1>Welcome to my portfolio!</h1>
+        </section>
+    )
+}
