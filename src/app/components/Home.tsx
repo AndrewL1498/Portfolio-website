d02fa2e2b07ceee86633ec308bcdbcc284070c26
AndrewL1498 <andrewlindsey498@gmail.com>
2026-03-27 02:58:29 -0500

    Getting home styling dialed in

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,13 +1,24 @@
 import styles from "../styles/home.module.css";
-
+import Image from "next/image";
 
 export default function Home() {
     return (
        <section id="home" className={styles.home}>
-            <h1>Hi, I'm Andrew!</h1>
-            <p>Frontend Developer & Software Engineer</p>
+            <div className={styles.textContainer}>
+                <h1>Hi, I'm Andrew</h1>
+                <span>Frontend Developer & Software Engineer</span>
+            </div>
 
-            <img></img>
+            <div className={styles.imageContainer}>
+            <Image
+                src="/images/me.png"
+                alt="Andrew Lindsey"
+                width={400} //adding width and height here as opposed to in css to prevent layout shift
+                height={400}
+                className={styles.profileImage}
+            />
+            </div>
+    
         </section>
     )
 }
