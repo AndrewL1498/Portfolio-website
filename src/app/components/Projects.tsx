4ea2b7cce5632f606eae9329be652614d9f5a294
AndrewL1498 <andrewlindsey498@gmail.com>
2026-04-16 17:30:13 -0500

    Completed adding info to the about, skills, and projects sections

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,10 +1,58 @@
 import styles from "../styles/projects.module.css";
+import Image from "next/image";
 
 
 export default function Projects() {
+
+    const projects = [
+        {
+            title: "Project 1",
+            Image: "https://i.imgur.com/mgIExN3.png",
+            link: "https://example.com/project1"
+        },
+        {
+            title: "Project 1",
+            Image: "https://i.imgur.com/EgO15cM.png",
+            link: "https://example.com/project1"
+        },
+        {
+            title: "Project 1",
+            Image: "https://i.imgur.com/VPT6Z0u.png",
+            link: "https://example.com/project1"
+        },
+        {
+            title: "Project 1",
+            Image: "https://i.imgur.com/dcoWQMr.png",
+            link: "https://example.com/project1"
+        }
+    ];
+
     return (
        <section id="projects" className={styles.projects}>
             <h1>My Projects</h1>
+
+        <div className={styles.grid}>
+          {projects.map((project, index) => (
+            <a
+              key={index}
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+
+                <Image 
+                src={project.Image} 
+                alt={project.title}
+                width={300}
+                height={200}
+                className={styles.image} 
+                />
+              </a>
+          ))}
+          
+
+        </div>
+
         </section>
     )
 }
--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -4,7 +4,16 @@ import styles from "../styles/skills.module.css";
 export default function Skills() {
     return (
        <section id="skills" className={styles.skills}>
-            <h1>My Skills</h1>
+            <h1>Skills</h1>
+
+        <ul className={styles.skillsGrid}>
+            <li>JAVASCRIPT</li>
+            <li>REACT</li>
+            <li>NODE.JS</li>
+            <li>POSTGRESQL</li>
+            <li>TYPESCRIPT</li>
+            <li>NEXT.JS</li>
+        </ul>
         </section>
     )
 }
